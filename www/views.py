acf1c7e9152d58b345700a7fd47cd71e1c0ad261
Michael Fogleman <fogleman@gmail.com>
2013-05-29 20:46:07 -0400

    added projects page

--- www/views.py
+++ www/views.py
@@ -8,6 +8,10 @@ import datetime
 def index():
     return render_template('index.html')
 
+@app.route('/projects/')
+def projects():
+    return render_template('projects.html')
+
 @app.route('/resume/')
 def resume():
     return render_template('resume.html')
