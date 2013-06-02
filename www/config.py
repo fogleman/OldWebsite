6e1f1d5ed249b9dec12e2351099f5386f5569a3a
Michael Fogleman <fogleman@gmail.com>
2013-06-01 21:40:33 -0400

    favicon, ext

--- www/config.py
+++ www/config.py
@@ -1,3 +1,4 @@
 SECRET_KEY = '91bf8d18f4c04ce28a908c9cdea7f6f5'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///www.db'
-STATIC_ROOT = None
+STATIC_ROOT = 'http://static.michaelfogleman.com/static/'
+EXT_ROOT = 'http://static.michaelfogleman.com/'
--- www/hooks.py
+++ www/hooks.py
@@ -4,11 +4,18 @@ import urlparse
 
 def static(path):
     root = app.config.get('STATIC_ROOT')
-    if root is None:
+    if app.debug or root is None:
+        return url_for('static', filename=path)
+    else:
+        return urlparse.urljoin(root, path)
+
+def ext(path):
+    root = app.config.get('EXT_ROOT')
+    if app.debug or root is None:
         return url_for('static', filename=path)
     else:
         return urlparse.urljoin(root, path)
 
 @app.context_processor
 def context_processor():
-    return dict(static=static)
+    return dict(static=static, ext=ext)
