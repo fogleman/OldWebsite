7d0bcc7f8c488052b96dfa5f3e68849ebbb6f916
Michael Fogleman <fogleman@gmail.com>
2013-12-30 21:50:41 -0500

    new static config

--- www/config.py
+++ www/config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = '91bf8d18f4c04ce28a908c9cdea7f6f5'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///www.db'
-STATIC_ROOT = 'http://static.michaelfogleman.com/static/'
-EXT_ROOT = 'http://static.michaelfogleman.com/'
+STATIC_ROOT = 'http://www.michaelfogleman.com/static/'
+EXT_ROOT = 'http://www.michaelfogleman.com/static/'
